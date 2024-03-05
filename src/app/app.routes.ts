5626bba249d0f56c9353902f5d4a659f834e5803
Eduardo Roth <eduardo@eduardoroth.dev>
2024-03-05 17:44:00 -0600

    fix(): update routes to simplify

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,20 +4,9 @@ export const routes: Routes = [
   {
     path: '',
     pathMatch: 'full',
-    redirectTo: 'pokemons',
-  },
-  {
-    path: 'pokemons',
     loadComponent: () =>
       import('./pages/+pokemon/pokemons/pokemons.page').then(
         (c) => c.PokemonsPage,
-      ),
-  },
-  {
-    path: 'pokemon',
-    loadComponent: () =>
-      import('./pages/+pokemon/pokemon/pokemon.page').then(
-        (c) => c.PokemonPage,
       ),
   },
 ];
